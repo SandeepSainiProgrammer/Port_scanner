06d814aa5bcb3f71d7c8efc428438454ba6bb9ce
RohitSaxsena <Acc3ssDenied001@gmail.com>
2014-06-02 20:14:19 -0700

    Create PortScanner.py

---
+++ PortScanner.py
@@ -0,0 +1,31 @@
+import sys
+from socket import *
+from threading import *
+
+
+
+print '\t\t\t ######################################'
+print '\t\t\t ##    Proudly Made By An INDIAN     ##'
+print '\t\t\t ##   Author : Rohit Saxsena INDIA   ##'
+print '\t\t\t ######################################'
+
+
+def threadScan(site,port) :
+    scan = socket(AF_INET, SOCK_STREAM)
+    connect = scan.connect_ex((site, port))
+    if (connect == 0) :
+        print  "[+]\tPort",port,": Open"
+    else : 
+        print "[-]\tPort",port,": Close" 
+    scan.close()
+    
+site = raw_input("Enter WebSite {www.target.com}: ")
+ports = raw_input("Enter Ports {21,22,80,....} : ")
+port = str(ports).split(',')
+ip = gethostbyname(site)
+print '  Your Target IP is :' +ip
+print '  Scanning :' +ip +'...\n'
+
+for ports in port:
+    t = Thread(target=threadScan, args=(site, int(ports)))
+    t.start()
